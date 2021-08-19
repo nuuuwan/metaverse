e5a098799c9e4f94389fb45457e8e69aa33a4d77
Nuwan I. Senaratna <nuuuwan@gmail.com>
2021-08-19 14:41:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# React App by nuuuwan
+
+[App Home](https://nuuuwan.github.io/nuwan_org)
+  * Updated README.md
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React App by nuuuwan</h1>
+    </div>
+  );
+}
+
+export default App;
